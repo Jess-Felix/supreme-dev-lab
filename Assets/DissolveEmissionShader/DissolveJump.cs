913e545c9c8b3b02c870c04a46ebf5f1f3f60ae1
Jessica <jessicateixeira32@gmail.com>
2019-11-03 22:17:07 +0000

    [upd] Trigger Effect and Dissolve Animation
    Added a trigger effect to the sphere ( mobile phone)  and increased the speed on the dissolve effect
    
    Added 2D fog effect on the last scene
    
    Resized the corridors
    
    Altered the player position on all scenes

--- Assets/DissolveEmissionShader/DissolveJump.cs
+++ Assets/DissolveEmissionShader/DissolveJump.cs
@@ -5,23 +5,28 @@ public class DissolveJump : MonoBehaviour {
     Material mat;
     Vector3[] floatingPositions;
     int index = 0;
-    bool hasJumped = false;
+    bool hasJumped = false; 
+    bool isEnabled = false;
+    private float animationTime = 0.0f;
     
     private void Start() {
         mat = GetComponent<Renderer>().material;
 
         floatingPositions = new Vector3[4];
-        floatingPositions[0] = transform.localPosition;
-        floatingPositions[1] = new Vector3(15.83f, 16.116f, -4.753f);
-        floatingPositions[2] = new Vector3(15.29f, 16.116f, -6.09f);
-        floatingPositions[3] = new Vector3(14.61f, 16.116f, -5.49f);
+        floatingPositions[3] = transform.localPosition;
+        floatingPositions[0] = new Vector3(15.83f, 16.116f, -4.753f);
+        floatingPositions[1] = new Vector3(15.29f, 16.116f, -6.09f);
+        floatingPositions[2] = new Vector3(14.61f, 16.116f, -5.49f);
 
     }
 
     private void Update() {
-        float dissolveAmount = Mathf.Sin(Time.time) / 2 + 0.5f;
-
-        if (dissolveAmount >= 0.99f)  {
+        if (!isEnabled) return;
+        
+        float dissolveAmount = Mathf.Sin(animationTime) / 2 + 0.5f;
+        animationTime += Time.deltaTime *3;
+        
+        if (dissolveAmount >= 0.99f )  {
             if (!hasJumped) {
                 transform.localPosition = floatingPositions[index];
                 IncIndex();
@@ -32,6 +37,7 @@ public class DissolveJump : MonoBehaviour {
             mat.SetFloat("_DissolveAmount", dissolveAmount);
         }
        
+        
     }
 
     private void IncIndex()
@@ -45,4 +51,13 @@ public class DissolveJump : MonoBehaviour {
             index=0;
         }
     }
-}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+
+            isEnabled = true;
+        }
+    }
+    }
