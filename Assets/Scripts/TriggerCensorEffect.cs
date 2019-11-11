943b667dda424b66d6bd8f93a60a0f99e553ff5d
Jessica <jessicateixeira32@gmail.com>
2019-11-11 17:31:27 +0000

    [upd] Third Room programming
    Deleted some files that weren't used.
    Added 3D models on first room
    Added texture with diary passage on third room.
    Added a censor effect triggered by the player.
    Working on the type animation still.

---
+++ Assets/Scripts/TriggerCensorEffect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerCensorEffect : MonoBehaviour
+{
+    bool isEnabled = false;
+    public GameObject pixel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        pixel.transform.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isEnabled)
+        {
+            pixel.transform.gameObject.SetActive(true);
+
+        }
+
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+
+            isEnabled = true;
+        }
+    }
+}
