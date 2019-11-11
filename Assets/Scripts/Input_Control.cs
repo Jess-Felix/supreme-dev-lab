e2fce135be7edf16a29f7d0fe3835e50fe0a4eaa
André Amorim <andre.leonard.amorim@gmail.com>
2019-11-11 23:06:44 +0000

    Sound
    Triggered sounds and atmosphere in rooms.

--- Assets/Scripts/Input_Control.cs
+++ Assets/Scripts/Input_Control.cs
@@ -6,12 +6,14 @@ public class Input_Control : MonoBehaviour
 {
     public GameObject vrCamera;
     private Rigidbody Rd;
+    public AudioSource walkingAudio;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        Rd = GetComponent<Rigidbody>();            
+        Rd = GetComponent<Rigidbody>();
+        walkingAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -20,6 +22,20 @@ public class Input_Control : MonoBehaviour
         if (Input.GetButton("Fire2")) {
             // this.transform.position += vrCamera.transform.forward * 2 * Time.deltaTime;
             Rd.MovePosition(transform.position + vrCamera.transform.forward * 2 * Time.deltaTime);
+
+            if (!walkingAudio.isPlaying)
+            {
+                walkingAudio.Play();
             }
+
+        }
+        else
+        {
+            walkingAudio.Stop();
+        }
+
+    
+        
+
     }
 }
