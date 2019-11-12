104e85bc193af669674cff95bc3e9a4176809b8a
Jessica <jessicateixeira32@gmail.com>
2019-11-12 20:52:59 +0000

    [upd] Animation Type
    Type appears, floats and moves towards the player.

---
+++ Assets/Scripts/TypeAnimation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TypeAnimation : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 1.0f;
+    private bool isEnabled = true;
+    private float timecounted = 0f;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isEnabled)
+        {
+            timecounted += Time.deltaTime;
+            Debug.Log("counting");
+        }
+
+        if (timecounted >= 2.5f)
+        { 
+            float step =  speed * Time.deltaTime; // calculate distance to move
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        }
+    }
+}
