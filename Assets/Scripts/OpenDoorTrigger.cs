99f04bb75f85143ff91737051b832de6467be744
Filipe Rodrigues <subrodrigues@gmail.com>
2019-11-14 00:49:28 +0000

    [new] Open Door Animation

---
+++ Assets/Scripts/OpenDoorTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoorTrigger : MonoBehaviour
+{
+    private Animation animation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animation = GetComponentInParent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            animation.Play("OpenDoor");
+        }
+    }
+}
