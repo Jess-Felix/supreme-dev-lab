070505e420a5c08f1c03fc765d86c4a437b5460a
Jessica <jessicateixeira32@gmail.com>
2019-11-14 02:36:09 +0000

    [new] Corridor 3 Content
    Wall and Arms interaction/animations

---
+++ Assets/Scripts/FloatForward.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatForward : MonoBehaviour
+{
+    float originalZ;
+
+    public float floatStrength = 1; // You can change this in the Unity Editor to 
+    // change the range of y positions that are possible.
+
+    void Start()
+    {
+        this.originalZ = this.transform.position.z;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x,
+           transform.position.y, originalZ + ((float)System.Math.Sin(Time.time) * floatStrength));
+    }
+}
